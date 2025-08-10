4449d67df062207193029e1c6b07d322b62c3ede
SARAN-S-04 <sarans0504@gmail.com>
2025-08-10 15:34:38 +0530

    cpp file

---
+++ index.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+    }
+
+    vector<int> b(m);
+    for (int i = 0; i < m; ++i) {
+        cin >> b[i];
+    }
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+        if (binary_search(b.begin(), b.end(), a[i])) {
+            ans++;
+        }
+    }
+
+    cout << ans << '\n';
+    
+    return 0;
+}
